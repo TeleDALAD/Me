a31233130f71688d155f460024301e77ef35cf3d
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-06 10:27:54 +0430

    Update timefa.lua

--- plugins/timefa.lua
+++ plugins/timefa.lua
@@ -1,7 +1,7 @@
 local address = 'http://powerful.friends-memorial.ir/api/'
 local function run(msg)
 local resolve = http.request(address..'index.php')
-resolve = string.gsub(resolve,'#Allwen','#ElementTG')
+resolve = string.gsub(resolve,'#Abol','#MegaTG')
 return resolve
 end
 return {
