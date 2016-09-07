ef36fb639477ae147718ce0374b0f3f542f7018c
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-07 16:09:56 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -3,11 +3,11 @@ do
 function run(msg, matches)
 local reply_id = msg['id']
 
-local info = '🌐 #Name : '..msg.from.first_name..'\n'
-..'🌐 #Your_Id : '..msg.from.id..'\n'
-..'🌐 #Your_Username : https://telegram.me/'..msg.from.username..'\n'
-..'🌐 #Group_Id : '..msg.to.id..'\n'
-..'🌐 #Group_name : '..msg.to.title
+local info = '#Name : '..msg.from.first_name..'\n'
+..'~Your Id : '..msg.from.id..'\n'
+..'~Your Username : telegram.me/'..msg.from.username..'\n'
+..'~Group Id : '..msg.to.id..'\n'
+..'~Group name : '..msg.to.title
 
 reply_msg(reply_id, info, ok_cb, false)
 end
