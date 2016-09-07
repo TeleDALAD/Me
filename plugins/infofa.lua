37a680ff8df0a4f75827699cf073204ac83fa027
Abolfazl Dalton <abolfazl.k.Dalton@gmail.com>
2016-09-07 15:55:07 +0430

    Update infofa.lua

--- plugins/infofa.lua
+++ plugins/infofa.lua
@@ -24,16 +24,16 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
 	local hash = 'rank:'..extra.chat2..':variables'
 	local value = redis:hget(hash, result.id)
     if not value then
-	if result.id == tonumber(SUDO) then
-	   text = text..'مقام : sudo⭐️⭐️⭐️⭐️⭐️  \n\n'
+	 if result.id == tonumber(SUDO) then
+	   text = text..'مقام : مدیر کل ربات (Executive Admin) \n\n'
 	  elseif is_admin2(result.id) then
-	   text = text..'مقام : Admin⭐️⭐️⭐️⭐️ \n\n'
+	   text = text..'مقام : ادمین ربات (Admin) \n\n'
 	  elseif is_owner2(result.id, extra.chat2) then
-	   text = text..'مقام : owner⭐️⭐️⭐️ \n\n'
+	   text = text..'مقام : مدیر کل گروه (Owner) \n\n'
 	  elseif is_momod2(result.id, extra.chat2) then
-	   text = text..'مقام : mod⭐️⭐️ \n\n'
+	    text = text..'مقام : مدیر گروه (Moderator) \n\n'
       else
-	   text = text..'مقام : Member⭐️ \n\n'
+	    text = text..'مقام : کاربر (Member) \n\n'
 	 end
    else
    text = text..'مقام : '..value..'\n\n'
@@ -43,7 +43,7 @@ local function res_user_callback(extra, success, result) -- /info <username> fun
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'@Tele_Mega'
+  text = text..'@Yalda_Robot'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
 	send_msg(extra.receiver, extra.user..' نام کاربری مورد نظر یافت نشد.', ok_cb, false)
@@ -64,16 +64,16 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local value = redis:hget(hash, result.id)
   if not value then
 	 if result.id == tonumber(SUDO) then
-	   text = text..'مقام : sudo⭐️⭐️⭐️⭐️⭐️  \n\n'
+	   text = text..'مقام : مدیر کل ربات (Executive Admin) \n\n'
 	  elseif is_admin2(result.id) then
-	   text = text..'مقام : Admin⭐️⭐️⭐️⭐️ \n\n'
+	   text = text..'مقام : ادمین ربات (Admin) \n\n'
 	  elseif is_owner2(result.id, extra.chat2) then
-	   text = text..'مقام : owner⭐️⭐️⭐️ \n\n'
+	   text = text..'مقام : مدیر کل گروه (Owner) \n\n'
 	  elseif is_momod2(result.id, extra.chat2) then
-	   text = text..'مقام : mod⭐️⭐️ \n\n'
-      else
-	   text = text..'مقام : Member⭐️ \n\n'
-	 end
+	   text = text..'مقام : مدیر گروه (Moderator) \n\n'
+	  else
+	   text = text..'مقام : کاربر (Member) \n\n'
+	  end
    else
     text = text..'مقام : '..value..'\n\n'
   end
@@ -82,7 +82,7 @@ local function action_by_id(extra, success, result)  -- /info <ID> function
   local um_hash = 'msgs:'..result.id..':'..extra.chat2
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'@Tele_Mega'
+  text = text..'@Yalda_Robot'
   send_msg(extra.receiver, text, ok_cb,  true)
   else
   send_msg(extra.receiver, 'ایدی شخص مورد نظر در سیستم ثبت نشده است.\nاز دستور زیر استفاده کنید\n/info @username', ok_cb, false)
@@ -101,17 +101,17 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
 	local hash = 'rank:'..result.to.id..':variables'
 		local value = redis:hget(hash, result.from.id)
 		 if not value then
-		   if result.id == tonumber(SUDO) then
-	   text = text..'مقام : sudo⭐️⭐️⭐️⭐️⭐️  \n\n'
-	  elseif is_admin2(result.id) then
-	   text = text..'مقام : Admin⭐️⭐️⭐️⭐️ \n\n'
-	  elseif is_owner2(result.id, extra.chat2) then
-	   text = text..'مقام : owner⭐️⭐️⭐️ \n\n'
-	  elseif is_momod2(result.id, extra.chat2) then
-	   text = text..'مقام : mod⭐️⭐️ \n\n'
-      else
-	   text = text..'مقام : Member⭐️ \n\n'
-	 end
+		    if result.from.id == tonumber(SUDO) then
+		       text = text..'مقام : مدیر کل ربات (Executive Admin) \n\n'
+		     elseif is_admin2(result.from.id) then
+		       text = text..'مقام : ادمین ربات (Admin) \n\n'
+		     elseif is_owner2(result.from.id, result.to.id) then
+		       text = text..'مقام : مدیر کل گروه (Owner) \n\n'
+		     elseif is_momod2(result.from.id, result.to.id) then
+		       text = text..'مقام : مدیر گروه (Moderator) \n\n'
+		 else
+		       text = text..'مقام : کاربر (Member) \n\n'
+			end
 		  else
 		   text = text..'مقام : '..value..'\n\n'
 		 end
@@ -150,7 +150,7 @@ local function run(msg, matches)
   return text
   end
   end
- if matches[1]:lower() == 'id' and not matches[2] then
+ if matches[1]:lower() == 'info' and not matches[2] then
   local receiver = get_receiver(msg)
   local Reply = msg.reply_id
   if msg.reply_id then
@@ -169,17 +169,17 @@ local function run(msg, matches)
 	if hash then
 	  local value = redis:hget(hash, msg.from.id)
 	  if not value then
-		if result.id == tonumber(SUDO) then
-	   text = text..'مقام : sudo⭐️⭐️⭐️⭐️⭐️  \n\n'
-	  elseif is_admin2(result.id) then
-	   text = text..'مقام : Admin⭐️⭐️⭐️⭐️ \n\n'
-	  elseif is_owner2(result.id, extra.chat2) then
-	   text = text..'مقام : owner⭐️⭐️⭐️ \n\n'
-	  elseif is_momod2(result.id, extra.chat2) then
-	   text = text..'مقام : mod⭐️⭐️ \n\n'
-      else
-	   text = text..'مقام : Member⭐️ \n\n'
-	 end
+		if msg.from.id == tonumber(SUDO) then
+		 text = text..'مقام : مدیر کل ربات (Executive Admin) \n\n'
+		elseif is_sudo(msg) then
+		 text = text..'مقام : ادمین ربات (Admin) \n\n'
+		elseif is_owner(msg) then
+		 text = text..'مقام : مدیر کل گروه (Owner) \n\n'
+		elseif is_momod(msg) then
+		 text = text..'مقام : مدیر گروه (Moderator) \n\n'
+		else
+		 text = text..'مقام : کاربر (Member) \n\n'
+		end
 	  else
 	   text = text..'مقام : '..value..'\n'
 	  end
@@ -198,7 +198,7 @@ local function run(msg, matches)
     return send_msg(receiver, text, ok_cb, true)
     end
   end
-  if matches[1]:lower() == 'id' and matches[2] then
+  if matches[1]:lower() == 'info' and matches[2] then
    local user = matches[2]
    local chat2 = msg.to.id
    local receiver = get_receiver(msg)
@@ -222,8 +222,8 @@ return {
 	'(Reply)!setrank <rank>: change members rank.',
   },
   patterns = {
-	"^[/!](id)$",
-	"^[/!](id) (.*)$",
+	"^[/!](info)$",
+	"^[/!](info) (.*)$",
 	"^[/!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (%d+) (.*)$",
 	"^[/!]([Ss][Ee][Tt][Rr][Aa][Nn][Kk]) (.*)$",
   },
